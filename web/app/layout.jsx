4ef6815aa88ac0f01a6e2433c0d8d720903cf794
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2024-03-08 10:40:26 +0700

    Change font & header rendering

---
+++ web/app/layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Montserrat } from 'next/font/google';
+
+import Header from './components/Header';
+
+import './globals.css';
+
+const montserrat = Montserrat({ subsets: ['cyrillic'] });
+
+export const metadata = {
+  title: 'Luce e Amore - свадебный салон',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='ru'>
+      <body className={montserrat.className}>
+        <main>
+          <Header />
+          <div className='max-w-6xl mx-auto pt-6'>{children}</div>
+        </main>
+      </body>
+    </html>
+  );
+}
