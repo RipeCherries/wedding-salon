62fa0ba338057e208b82846cdb3066010e2ddded
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2024-03-17 19:19:43 +0700

    Added new colors

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './catalog/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
@@ -11,8 +11,16 @@ module.exports = {
         'main': '#fcf9f4',
         'primary': '#ead5d4',
         'secondary': '#E7D6C0',
+        'quaternary': '#463a3c',
       },
       textColor: {
+        'primary': '#463a3c',
+        'secondary': '#fcf9f4',
+      },
+      borderColor: {
+        'primary': '#463a3c',
+      },
+      placeholderColor: {
         'primary': '#463a3c',
       },
     },
