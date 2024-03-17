0028edf3f4ce782ff43da488af849d1803403277
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2024-03-17 21:03:22 +0700

    Added export of all sections

---
+++ web/app/sections/index.js
@@ -0,0 +1,8 @@
+import MenuSection from './MenuSection';
+import AssortmentSection from './AssortmentSection';
+import AboutSection from './AboutSection';
+import AdvantagesSection from './advantages/AdvantagesSection';
+import FeedbackSection from './feedback/FeedbackSection';
+import HandMadeSection from './HandMadeSection';
+
+export { MenuSection, AssortmentSection, AboutSection, AdvantagesSection, FeedbackSection, HandMadeSection };
