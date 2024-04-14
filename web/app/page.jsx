ed08adf063e7650eb97da50032576b0f93ff56c5
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2024-04-14 21:01:39 +0700

    Changed the logic of rendering the main page

--- web/app/page.jsx
+++ web/app/page.jsx
@@ -7,6 +7,66 @@ import {
   FeedbackSection,
   HandMadeSection,
 } from './sections';
+import { GiAmpleDress, GiReceiveMoney } from 'react-icons/gi';
+import { FaMedal } from 'react-icons/fa';
+import { BiSolidChart, BiSolidUserCheck } from 'react-icons/bi';
+import { IoCalendar, IoResizeOutline } from 'react-icons/io5';
+import { GrLocation } from 'react-icons/gr';
+import { BsListStars } from 'react-icons/bs';
+import { MdFeedback } from 'react-icons/md';
+
+const advantages = [
+  {
+    id: 1,
+    icon: <GiAmpleDress size={52} style={{ fill: '#463a3c' }} />,
+    text: 'Уникальный дизайн и стиль',
+  },
+  {
+    id: 2,
+    icon: <FaMedal size={52} style={{ fill: '#463a3c' }} />,
+    text: 'Высокое качество услуг',
+  },
+  {
+    id: 3,
+    icon: <BiSolidUserCheck size={52} style={{ fill: '#463a3c' }} />,
+    text: 'Индивидуальный подход к каждому клиенту',
+  },
+  {
+    id: 4,
+    icon: <IoResizeOutline size={52} style={{ fill: '#463a3c' }} />,
+    text: 'Широкий выбор моделей и размеров',
+  },
+  {
+    id: 5,
+    icon: <BiSolidChart size={52} style={{ fill: '#463a3c' }} />,
+    text: 'Профессиональные консультанты',
+  },
+  {
+    id: 6,
+    icon: <GrLocation size={52} style={{ fill: '#463a3c' }} />,
+    text: 'Удобное расположение и уютная атмосфера',
+  },
+  {
+    id: 7,
+    icon: <BsListStars size={52} style={{ fill: '#463a3c' }} />,
+    text: 'Полный спектр услуг',
+  },
+  {
+    id: 8,
+    icon: <MdFeedback size={52} style={{ fill: '#463a3c' }} />,
+    text: 'Положительные отзывы и репутация',
+  },
+  {
+    id: 9,
+    icon: <GiReceiveMoney size={52} style={{ fill: '#463a3c' }} />,
+    text: 'Доступные цены и специальные предложения',
+  },
+  {
+    id: 10,
+    icon: <IoCalendar size={52} style={{ fill: '#463a3c' }} />,
+    text: 'Гибкий график и удобные условия записи',
+  },
+];
 
 export default function Home() {
   return (
@@ -14,7 +74,7 @@ export default function Home() {
       <MenuSection />
       <AssortmentSection />
       <AboutSection />
-      <AdvantagesSection />
+      {/*<AdvantagesSection advantages={advantages} type='mainPage' />*/}
       <FeedbackSection />
       <HandMadeSection />
     </main>
