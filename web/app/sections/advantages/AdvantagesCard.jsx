8a290e49a0ee6f184c218621915b9787f2c4368e
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2024-04-15 21:16:00 +0700

    Update AdvantagesCard.jsx

--- web/app/sections/advantages/AdvantagesCard.jsx
+++ web/app/sections/advantages/AdvantagesCard.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function AdvantagesCard({ reason }) {
+export default function AdvantagesCard({ advantage, type }) {
   return (
     <div className='min-w-56 max-w-56 p-6 rounded-2xl flex flex-col items-center'>
-      <div className='w-28 h-28 rounded-full bg-main flex justify-center items-center mb-2'>{reason.icon}</div>
-      <p className='text-primary font-semibold text-s text-center'>{reason.text}</p>
+      <div
+        className={`w-28 h-28 rounded-full ${type === 'mainPage' ? 'bg-main' : 'bg-primary'} flex justify-center items-center mb-2`}
+      >
+        {advantage.icon}
+      </div>
+      <p className='text-primary font-semibold text-s text-center'>{advantage.text}</p>
     </div>
   );
 }
 
 AdvantagesCard.propTypes = {
-  reason: PropTypes.shape({
+  advantage: PropTypes.shape({
     icon: PropTypes.node.isRequired,
     text: PropTypes.string.isRequired,
   }).isRequired,
+  type: PropTypes.string.isRequired,
 };
