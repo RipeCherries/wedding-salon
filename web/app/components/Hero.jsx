aa3fcfcf206ca0b6fce5861af0c299c9af2e78b2
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2024-04-30 10:08:19 +0700

    Changed the Hero section

--- web/app/components/Hero.jsx
+++ web/app/components/Hero.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 export default function Hero() {
   return (
-    <section
-      className='bg-no-repeat bg-cover bg-bottom bg-fixed relative'
-      style={{ backgroundImage: `url('/images/hero.jpg')` }}
-    >
-      <div className='backdrop-blur-sm w-full h-full absolute top-0 left-0 bg-orange-50 bg-opacity-50' />
-      <div className='max-w-6xl mx-auto py-16 z-10 relative'>
-        <h1 className='text-5xl font-bold text-justify text-primary'>Ждём Вас!</h1>
-        <h2 className='text-lg mt-4 text-justify text-primary font-bold'>
-          по адресу: г. Новосибирск, ул. Семьи Шамшиных, д. 64, к. 211
-        </h2>
-        <h2 className='text-lg mt-1 text-justify text-primary font-bold'>
-          тел.{' '}
-          <a className='underline' href='tel:+79833058585'>
-            +7-983-305-85-85
-          </a>
+    <div className='relative'>
+      <video autoPlay muted loop className='absolute inset-0 w-full h-full object-cover'>
+        <source src="/video/hero.mp4" type="video/mp4" />
+        Ваш браузер не поддерживает видео.
+      </video>
+      <div className='w-full h-full absolute top-0 left-0 from-gradientStart to-gradientEnd bg-gradient-to-b' />
+      <div className='max-w-6xl mx-auto flex flex-col items-center relative z-10 py-10'>
+        <img
+          src="/images/logo.svg"
+          alt="Логотип свадебного салона Luce e Amore"
+          className='w-72'
+        />
+        <h1 className='text-5xl font-semibold text-justify text-primary second__font'>Салон свадебных платьев</h1>
+        <h2 className='text-2xl mt-4 text-justify text-primary font-semibold second__font'>
+          Место, где любовь светит ярче
         </h2>
       </div>
-    </section>
+    </div>
   );
 }
