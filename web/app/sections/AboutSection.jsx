48266f741a2972e6fb765ee23888caa63c31c38a
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2024-05-13 18:07:14 +0700

    Update images

--- web/app/sections/AboutSection.jsx
+++ web/app/sections/AboutSection.jsx
@@ -1,40 +1,6 @@
-'use client';
-
-import React, { useState } from 'react';
-import { MdNavigateNext } from 'react-icons/md';
-import { FaPlay } from 'react-icons/fa';
-import VideoModal from '../components/VideoModal';
-
-const saloonImages = [
-  'https://nicolewedding.ru/upload/resize_cache/iblock/e31/720_548_2/e316d7ae5001a873d227c7ba397e77e8.jpg',
-  'https://nicolewedding.ru/upload/resize_cache/iblock/9e3/720_548_2/9e33ff2ca415b699a21269db28d976e1.jpg',
-  'https://nicolewedding.ru/upload/resize_cache/iblock/b9c/720_548_2/b9c97078f25f74a470905b76c098e7b7.jpg',
-  'https://nicolewedding.ru/upload/resize_cache/iblock/07a/720_548_2/07aeff22845354ea8a86cbd9c14da66a.jpg',
-  'https://nicolewedding.ru/upload/resize_cache/iblock/7d3/720_548_2/7d3477649123dfe95bde285027985637.jpg',
-  'https://nicolewedding.ru/upload/resize_cache/iblock/75e/720_548_2/75e826ca5f4319f35bfd40027d6b010f.jpg',
-  'https://nicolewedding.ru/upload/resize_cache/iblock/c9e/720_548_2/c9e94a0ca76c5b6c9427b4ed54b49370.jpg',
-];
+import React from 'react';
 
 export default function AboutSection() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [videoModalIsOpen, setVideoModalIsOpen] = useState(false);
-
-  const previousSlide = () => {
-    if (currentIndex === 0) {
-      setCurrentIndex(saloonImages.length - 1);
-    } else {
-      setCurrentIndex(currentIndex - 1);
-    }
-  };
-
-  const nextSlide = () => {
-    if (currentIndex === saloonImages.length - 1) {
-      setCurrentIndex(0);
-    } else {
-      setCurrentIndex(currentIndex + 1);
-    }
-  };
-
   return (
     <section className='xl:py-10 py-4'>
       <div className='max-w-6xl mx-auto'>
@@ -47,48 +13,9 @@ export default function AboutSection() {
           <div className='xl:w-1/2'>
             <div className='overflow-hidden relative xl:mt-12'>
               <div
-                className='flex transition ease-out duration-700'
-                style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+                className="flex transition ease-out duration-700"
               >
-                {saloonImages.map((item, index) => (
-                  <img
-                    src={item}
-                    key={`image-${index * 1488}`}
-                    alt='Фоточка салона'
-                    className='rounded-xl'
-                  />
-                ))}
-              </div>
-              <button
-                onClick={previousSlide}
-                type='button'
-                className='absolute top-1/2 left-2 transform -translate-y-1/2 bg-quaternary p-2 rounded-full'
-              >
-                <MdNavigateNext className='rotate-180' size={32} style={{ fill: '#fcf9f4' }} />
-              </button>
-              <button
-                onClick={nextSlide}
-                type='button'
-                className='absolute top-1/2 right-2 transform -translate-y-1/2 bg-quaternary p-2 rounded-full'
-              >
-                <MdNavigateNext size={32} style={{ fill: '#fcf9f4' }} />
-              </button>
-              <button
-                onClick={() => setVideoModalIsOpen((prevState) => !prevState)}
-                type='button'
-                className='absolute top-6 left-6 px-4 py-2 bg-main rounded-lg'
-              >
-                <FaPlay size={20} style={{ fill: '#463a3c' }} />
-              </button>
-              <div className='absolute bottom-2 w-full flex justify-center gap-4'>
-                {saloonImages.map((item, i) => (
-                  <button
-                    onClick={() => setCurrentIndex(i)}
-                    type='button'
-                    key={`circle${i * 50}`}
-                    className={`cursor-pointer w-3 h-3 rounded-full ${i === currentIndex ? 'bg-quaternary' : 'bg-main'}`}
-                  />
-                ))}
+                <img src="/images/about.jpg" alt="about luce e amore" className="rounded-xl" />
               </div>
             </div>
           </div>
@@ -125,12 +52,6 @@ export default function AboutSection() {
             </a>
           </div>
         </div>
-        {videoModalIsOpen && (
-          <VideoModal
-            url='https://www.youtube.com/embed/dQw4w9WgXcQ?si=4vlQ1pEM1V_9S1Gh'
-            onClose={() => setVideoModalIsOpen((prevState) => !prevState)}
-          />
-        )}
       </div>
     </section>
   );
--- web/app/sections/HandMadeSection.jsx
+++ web/app/sections/HandMadeSection.jsx
@@ -1,41 +1,8 @@
-'use client';
-
-import React, { useState } from 'react';
-import { MdNavigateNext } from 'react-icons/md';
-import { FaPlay } from 'react-icons/fa';
+import React from 'react';
 import Link from 'next/link';
-import VideoModal from '../components/VideoModal';
 
-const saloonImages = [
-  'https://nicolewedding.ru/upload/resize_cache/iblock/e31/720_548_2/e316d7ae5001a873d227c7ba397e77e8.jpg',
-  'https://nicolewedding.ru/upload/resize_cache/iblock/9e3/720_548_2/9e33ff2ca415b699a21269db28d976e1.jpg',
-  'https://nicolewedding.ru/upload/resize_cache/iblock/b9c/720_548_2/b9c97078f25f74a470905b76c098e7b7.jpg',
-  'https://nicolewedding.ru/upload/resize_cache/iblock/07a/720_548_2/07aeff22845354ea8a86cbd9c14da66a.jpg',
-  'https://nicolewedding.ru/upload/resize_cache/iblock/7d3/720_548_2/7d3477649123dfe95bde285027985637.jpg',
-  'https://nicolewedding.ru/upload/resize_cache/iblock/75e/720_548_2/75e826ca5f4319f35bfd40027d6b010f.jpg',
-  'https://nicolewedding.ru/upload/resize_cache/iblock/c9e/720_548_2/c9e94a0ca76c5b6c9427b4ed54b49370.jpg',
-];
 
 export default function HandMadeSection() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [videoModalIsOpen, setVideoModalIsOpen] = useState(false);
-
-  const previousSlide = () => {
-    if (currentIndex === 0) {
-      setCurrentIndex(saloonImages.length - 1);
-    } else {
-      setCurrentIndex(currentIndex - 1);
-    }
-  };
-
-  const nextSlide = () => {
-    if (currentIndex === saloonImages.length - 1) {
-      setCurrentIndex(0);
-    } else {
-      setCurrentIndex(currentIndex + 1);
-    }
-  };
-
   return (
     <section className='xl:py-10 py-4 bg-primary'>
       <div className='max-w-6xl mx-auto'>
@@ -49,47 +16,8 @@ export default function HandMadeSection() {
             <div className='overflow-hidden relative'>
               <div
                 className='flex transition ease-out duration-700'
-                style={{ transform: `translateX(-${currentIndex * 100}%)` }}
               >
-                {saloonImages.map((item, index) => (
-                  <img
-                    src={item}
-                    key={`image-${index * 1488}`}
-                    alt="Фоточка салона"
-                    className="rounded-xl"
-                  />
-                ))}
-              </div>
-              <button
-                onClick={previousSlide}
-                type='button'
-                className='absolute top-1/2 left-2 transform -translate-y-1/2 bg-quaternary p-2 rounded-full'
-              >
-                <MdNavigateNext className='rotate-180' size={32} style={{ fill: '#fcf9f4' }} />
-              </button>
-              <button
-                onClick={nextSlide}
-                type='button'
-                className='absolute top-1/2 right-2 transform -translate-y-1/2 bg-quaternary p-2 rounded-full'
-              >
-                <MdNavigateNext size={32} style={{ fill: '#fcf9f4' }} />
-              </button>
-              <button
-                onClick={() => setVideoModalIsOpen((prevState) => !prevState)}
-                type='button'
-                className='absolute top-6 left-6 px-4 py-2 bg-main rounded-lg'
-              >
-                <FaPlay size={20} style={{ fill: '#463a3c' }} />
-              </button>
-              <div className='absolute bottom-2 w-full flex justify-center gap-4'>
-                {saloonImages.map((item, i) => (
-                  <button
-                    onClick={() => setCurrentIndex(i)}
-                    type='button'
-                    key={`circle${i * 51}`}
-                    className={`cursor-pointer w-3 h-3 rounded-full ${i === currentIndex ? 'bg-quaternary' : 'bg-main'}`}
-                  />
-                ))}
+                <img src="/images/hand-made.jpg" alt="hand-made luce e amore" className="rounded-xl" />
               </div>
             </div>
           </div>
@@ -113,12 +41,6 @@ export default function HandMadeSection() {
             </Link>
           </div>
         </div>
-        {videoModalIsOpen && (
-          <VideoModal
-            url='https://www.youtube.com/embed/dQw4w9WgXcQ?si=4vlQ1pEM1V_9S1Gh'
-            onClose={() => setVideoModalIsOpen((prevState) => !prevState)}
-          />
-        )}
       </div>
     </section>
   );
